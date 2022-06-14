d8339692ea1b5438b4f5b3ad9a0c64d76e490905
Roleeygit <juhaszroli2001@gmail.com>
2022-06-14 09:02:06 +0200

    A kocsik a pályán!

--- src/views/Car.java
+++ src/views/Car.java
@@ -5,18 +5,20 @@ import java.awt.Color;
 import javax.swing.JButton;
 
 public class Car extends JButton{
-    Color color;
+    // Color color;
     Integer actSpeed;
     int maxSpeed;
     boolean speedVisible;
-    String name;
+    // String name;
 
     public Car(Color color, int initSpeed, String name) {
-        this.color = color;
+        // this.color = color;
+        this.setBackground(color);
+        this.setText(name);
         this.actSpeed = initSpeed;
         this.maxSpeed = 20;
         this.speedVisible = true;
-        this.name = name;
+        // this.name = name;
     }
     public Integer getActSpeed() {
         return this.actSpeed;
