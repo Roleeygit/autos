8f3fe0fc39ddcbeb5acf99aaede2ed4fa6e6b576
Roleeygit <juhaszroli2001@gmail.com>
2022-06-14 08:49:42 +0200

    .

--- src/views/TrackPanel.java
+++ src/views/TrackPanel.java
@@ -8,7 +8,7 @@ public class TrackPanel extends JPanel {
     Car car1;
     public TrackPanel() {
         this.car1 = new Car(Color.BLUE, 5, "test1");
-
+        
         this.add(this.car1);
         this.setBounds(5, 5, 750, 150);
         this.setLayout(null);
